f825a7cfee49d9d4d66a55d25bac4527dfa5bc8c
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-06-19 22:40:56 +0200

    initialize project

---
+++ src/test/java/es/ugr/student/StudentAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ugr.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
